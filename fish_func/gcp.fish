c99dbe29dd40b979d2ce7ecb4a179a54cdaa6399
Chris Chu <c20711@gmail.com>
2023-08-14 10:37:34 +0800

    Add gssh

--- fish_func/gcp.fish
+++ fish_func/gcp.fish
@@ -26,3 +26,23 @@ function gdigx
     # Execute the gcloud command
     gcloud dns record-sets list --zone=sinyi-com-tw --filter="DATA:$IP"
 end
+
+function gssh
+    set name $argv[1]
+    if [ -z "$name" ]
+        echo "Usage: list_instance <instance-name>"
+        return
+    end
+
+    # 使用 gcloud 查詢虛擬機實例
+    set instance_info (gcloud compute instances list --filter=name:$name --format="value(name,zone)")
+    
+    if [ -n "$instance_info" ]
+        set -l instance_name (echo $instance_info | awk '{print $1}')
+        set -l zone (echo $instance_info | awk '{print $2}')
+        
+        gcloud compute ssh --zone $zone $instance_name --project "sinyi-cloud" --tunnel-through-iap
+    else
+        echo "No instance found with the name '$name'."
+    end
+end    
